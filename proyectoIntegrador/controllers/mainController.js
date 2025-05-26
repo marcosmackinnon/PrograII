abeccc4c9b8f6be7785c5a2f447d7bd521593d4e
rockysasso <fernandezsassos@udesa.edu.ar>
2025-05-26 19:45:16 -0300

    cambie el mainController

--- proyectoIntegrador/controllers/mainController.js
+++ proyectoIntegrador/controllers/mainController.js
@@ -1,22 +1,26 @@
-let data = require("../db/usersData.js");
+const db = require("../database/models");
 
 const mainController = {
-    index: function(req, res) {
-      res.render('index', { title: 'Mercedez Benz Store', data: data }); //va a replazar el title en <%= title %> del index.ejs  
-    },
+  index: function(req, res) {
+    db.Producto.findAll({
+      include: [{ association: "usuario" }]
+    })
+    .then(function(productos) {
+      res.render("index", {
+        title: "Mercedez Benz Store",
+        data: productos
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  },
 
-    searchResults: function(req, res) {
-      res.render('search-results', { title: 'Mercedez Benz Store', data: data }); 
-    },
-
-    profile: function(req, res) {
-      res.render('profile', { title: 'Mercedez Benz Store', data: data }); 
-    },
-
-    
-  };
-
-  module.exports = mainController;
-
+  searchResults: function(req, res) {
+    db.Producto.findAll({
+      include: [{ association: "usuario" }]
+    })
+  }
+}
 
  
